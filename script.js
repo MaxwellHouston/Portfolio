f5f9016e8555333c58c903a11e01494c38af87a3
MaxwellHouston <87088784+MaxwellHouston@users.noreply.github.com>
2021-08-04 17:08:44 -0400

    added files for code veiwer and started javascript file

---
+++ script.js
@@ -0,0 +1,11 @@
+/*Buttons*/
+
+const aboutButton = document.getElementById('about-button');
+const gameButton = document.getElementById('game-button');
+const islandButton = document.getElementById('island-button');
+const portfolioButton = document.getElementById('portfolio-button');
+const moreButton = document.getElementById('more-button');
+const imgButton = document.getElementById('img-button');
+const htmlButton = document.getElementById('html-button');
+const cssButton = document.getElementById('css-button');
+const scriptButton = document.getElementById('script-button');
