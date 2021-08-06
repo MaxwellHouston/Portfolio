465a35a50ea2557c33cd57404b0003ecf2c87f1a
MaxwellHouston <87088784+MaxwellHouston@users.noreply.github.com>
2021-08-06 19:34:20 -0400

    added goToArticle function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let page = 'aboutArticle';
+
 /*Buttons*/
 
 const aboutButton = document.getElementById('about-button');
@@ -26,34 +28,72 @@ const reset = () => {
     islandButton.style.textDecoration = 'none';
     portfolioButton.style.textDecoration = 'none';
     moreButton.style.textDecoration = 'none';
+    aboutArticle.style.zIndex = 1;
+    gameArticle.style.zIndex = 1;
+    islandArticle.style.zIndex = 1;
+    portfolioArticle.style.zIndex = 1;
+    moreArticle.style.zIndex = 1;
 }
 
 const highlight = (button) => {
     button.style.textDecoration= 'underline #4266a1';
 }
 
+/*Nav Bar Articles*/
+
+const goToArticle = (article) => {
+    article.style.zIndex = 2;
+}
+
 /*About Button*/
+
 aboutButton.addEventListener('click', reset)
 aboutButton.addEventListener('click', function () {
     highlight(aboutButton);
 })
+aboutButton.addEventListener('click', function () {
+    goToArticle(aboutArticle);
+} )
+
 /*Game Button*/
+
 gameButton.addEventListener('click', reset);
 gameButton.addEventListener('click', function () {
     highlight(gameButton);
 })
+gameButton.addEventListener('click', function () {
+    goToArticle(gameArticle);
+})
+
 /*Island Button*/
+
 islandButton.addEventListener('click', reset);
 islandButton.addEventListener('click', function () {
     highlight(islandButton);
 })
+islandButton.addEventListener('click', function () {
+    goToArticle(islandArticle);
+})
+
 /*Portfolio Button*/
+
 portfolioButton.addEventListener('click', reset);
 portfolioButton.addEventListener('click', function () {
     highlight(portfolioButton);
 })
+portfolioButton.addEventListener('click', function () {
+    goToArticle(portfolioArticle);
+})
+
 /*More Button*/
+
 moreButton.addEventListener('click', reset);
 moreButton.addEventListener('click', function () {
     highlight(moreButton);
 })
+moreButton.addEventListener('click', function() {
+    goToArticle(moreArticle);
+})
+
+/*Code Viewer*/
+
